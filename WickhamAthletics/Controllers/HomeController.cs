49bb9caff8ce1dbfa7c7f1fcd0b095002c4c97db
Travis <twickham37@gmail.com>
2016-05-18 21:38:11 -0500

    Add project files.

---
+++ WickhamAthletics/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WickhamAthletics.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Check our prices and contact us for more information.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Ryan()
+        {
+            return View();
+        }
+
+        public ActionResult Jimmy()
+        {
+            return View();
+        }
+        
+    }
+}
